f6fd1e822cf2c678eb91e32018f45f2ddf875ef2
Roberts Studens <robertsstudens@gmail.com>
2018-02-13 21:36:23 +0200

    1.lesson - Hello world

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+       {
+         
+            // izsaucam sevis izveidoto funkciju
+            SayHello();
+            // izsaucam otru izveidoto funkciju
+            SayHelloToUser();
+        }
+
+        static void SayHelloToUser()
+        {
+            Console.WriteLine("Please enter your name:");
+            string usersName;
+            usersName = Console.ReadLine();
+            Console.WriteLine("Labdien," + usersName + "!");
+            Console.ReadLine();
+
+
+        }
+
+        static void SayHello()
+        {
+           
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
