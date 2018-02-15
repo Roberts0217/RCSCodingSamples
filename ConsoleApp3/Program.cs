7164d878d11c02c1b455ba7dbba1a946fad7a285
Roberts Studens <robertsstudens@gmail.com>
2018-02-15 21:32:06 +0200

    spele

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //izveidojam mainīgo kur glabāt skaitļu ģenerātoru instanci
+            Random diceNumberGenerator;
+            //ierakstam mainīhajam jaunizveidoto ģenerātoru
+            diceNumberGenerator = new Random();
+            //paprasam ģenerātoram iedot skaitli starp 1 un 7,
+            //ierakstam rezultātu mainīgajā
+            int diceValue = diceNumberGenerator.Next(1, 7);
+            Console.WriteLine("Tavs kauliņa metiens " + diceValue);
+            int secondDiceValue = diceNumberGenerator.Next(0, 101);
+            Console.WriteLine("Tavs kauliņa metiens " + secondDiceValue);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //paprasīt lietotājam, cik lielu skaitli viņš grib minēt uzģenerēt gadījuma skaitli līdz šai robežai
+            Console.WriteLine("Izvelēties skaitli, ko minēt");
+            int maxNumber = int.Parse(Console.ReadLine());
+            Random diceNumberGenerator;
+            diceNumberGenerator = new Random();
+            //uzģenerēt gadījuma skaitli līdz šai robežai
+            int guess = diceNumberGenerator.Next(1, maxNumber);
+            //cikls: kamēr lietotājs neuzmin:
+            string 
+            while (true)
+            {
+                if (maxNumber == diceNumberGenerator)
+                {
+                    break;
+                }
+               
+            }
+
+            //paprasīt lietotājam lai viņš min kāds skaitlis ir izveidots (iegūt ievadi)
+            //salīdzināt, vai lietotājs ir uzminējis ja ir, tad izbeigt
+
+        }
+    }
+}
